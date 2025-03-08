609010f2c161aedfedf67306c51d1e83a7ecffe8
“Zeeshaun <zahmad28@myseneca.ca>
2025-03-07 20:39:23 -0500

    final commit

--- components/MainNav.js
+++ components/MainNav.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
@@ -6,6 +8,16 @@ import Navbar from 'react-bootstrap/Navbar';
 import Link from 'next/link';
 
 export default function MainNav() {
+    const [searchField, setSearchField] = useState('');
+    const router = useRouter();
+
+    const handleSearch = (event) => {
+        event.preventDefault();
+        if (searchField.trim()) {
+            router.push(`/artwork?title=true&q=${searchField}`);
+        }
+    };
+
     return (
         <>
             <Navbar className="fixed-top navbar-dark bg-primary">
@@ -19,14 +31,16 @@ export default function MainNav() {
                             <Nav.Link>Advanced Search</Nav.Link>
                         </Link>
                     </Nav>
-                    <Form className="d-flex">
-                        <Form.Control
+                    <Form className="d-flex" onSubmit={handleSearch}>
+                    <Form.Control
                             type="search"
                             placeholder="Search"
                             className="me-2"
                             aria-label="Search"
+                            value={searchField}
+                            onChange={(e) => setSearchField(e.target.value)}
                         />
-                        <Button variant="outline-success">Search</Button>
+                        <Button type="submit" variant="outline-success">Search</Button>
                     </Form>
                 </Container>
             </Navbar>
